32f1661aad99ed0a830288889cfe75114642ab5c
kjgilchrist <93557317+kjgilchrist@users.noreply.github.com>
2021-12-13 17:13:09 -0700

    Create crys-dict.js

---
+++ sketches/lattice-music/crys-dict.js
@@ -0,0 +1,18 @@
+/*
+  Reference: https://en.wikipedia.org/wiki/Crystal_structure
+*/
+
+var TETRAHEDRON = {
+  vertices: 4,
+  midis: [60, 64, 67, 69]
+}
+
+var PYRAMID = {
+  vertices: 5,
+  midis: [60, 64, 65, 67, 69]
+}
+
+var CUBOID = {
+  vertices: 8,
+  midis: [60, 62, 64, 65, 67, 69, 71, 72]
+}
